36e1cc6851caa94e6a97e45c1783cb7b7baf31e2
Timirov Rail <timirov.rail@inbox.ru>
2024-12-31 18:09:33 +0300

    leave feedback

--- app/Reviews/page.tsx
+++ app/Reviews/page.tsx
@@ -1,11 +1,13 @@
 import Header from '../../components/Header/Header';
 import SliderReviewsClient from '../../components/SliderReviewsClient/SliderReviewsClient';
+import LeaveFeedback from '../../components/LeaveFeedback/LeaveFeedback';
 
 export default function Reviews() {
     return (
         <>
             <Header />
             <SliderReviewsClient />
+            <LeaveFeedback />
         </>
     );
 }
