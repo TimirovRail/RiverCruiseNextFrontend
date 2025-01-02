1ce949b989d489897fc322ec3bfa5e036b3a0b30
Timirov Rail <timirov.rail@inbox.ru>
2025-01-02 22:15:01 +0300

    added restaurant and store pages

---
+++ components/BlockRestaurant/BlockRestaurant.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BlockRestaurant.module.css';
+
+const BlockRestaurant = () => {
+    return (
+        <div className='layout'>
+            <div className='title'>
+                <h2 className='h1-title'>РЕСТОРАН</h2>
+            </div>
+            <h1>Ресторан, еда, оздоровление, массаж, развлечение</h1>
+        </div>
+    );
+};
+
+export default BlockRestaurant;
---
+++ components/BlockShop/BlockShop.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BlockShop.module.css';
+
+const BlockShop = () => {
+    return (
+        <div className='layout'>
+            <div className='title'>
+                <h2 className='h1-title'>МАГАЗИН</h2>
+            </div>
+            <h1>Карточки товаров, сувениры, магнитики и тд</h1>
+        </div>
+    );
+};
+
+export default BlockShop;
